1e8e77f2975064f1108cc06b09a36b1ff4904e9c
Dinko Krastev <dinko.krastev@progress.com>
2026-04-14 16:58:10 +0300

    Fix error

--- snippets/Telerik.WinControls.UI.Snippets/Common.VB/Form1.vb
+++ snippets/Telerik.WinControls.UI.Snippets/Common.VB/Form1.vb
@@ -4,7 +4,7 @@ Public Class Form1
 
 	'// >> aiprompt-getting-started-promptrequest-vb
 	Private Sub RadAIPrompt1_PromptRequest(ByVal sender As Object, ByVal e As Telerik.WinControls.UI.AIPrompt.PromptRequestEventArgs)
-		Dim responseAIPromptOutputItemModel As AIPrompt OutputItem = New AIPromptOutputItem() With {
+		Dim responseAIPromptOutputItemModel As AIPromptOutputItem = New AIPromptOutputItem() With {
 			.Title = "Response from your AI model",
 			.InputText = e.InputText,
 			.ResponseText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
